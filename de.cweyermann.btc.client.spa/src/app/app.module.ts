fd172e1008adafc6c6506b76b72d8e80c5838bc3
chris <github@cweyermann.de>
2017-03-15 17:35:21 +0100

    set up routing

--- de.cweyermann.btc.client.spa/src/app/app.module.ts
+++ de.cweyermann.btc.client.spa/src/app/app.module.ts
@@ -6,20 +6,29 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { ClubstandingsComponent } from './clubstandings/clubstandings.component';
+import { GroupsComponent } from './groups/groups.component';
 import { MatchesComponent } from './matches/matches.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
+    GroupsComponent,
     ClubstandingsComponent,
     MatchesComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
-  ],
+    HttpModule,
+    RouterModule.forRoot([
+      {path: 'clubs', component: ClubstandingsComponent},
+      {path: 'matches', component: MatchesComponent},
+      {path: 'groups/:id', component: GroupsComponent},
+      {path: 'welcome', component: WelcomeComponent},
+      {path: '', redirectTo: 'welcome', pathMatch: 'full'}
+    ])],
   providers: [],
   bootstrap: [AppComponent]
 })
