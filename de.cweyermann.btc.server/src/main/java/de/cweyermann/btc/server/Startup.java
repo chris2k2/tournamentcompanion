a44d364efe6b5d6ed6d7b55eecfa75e0f18cc99d
chris <github@cweyermann.de>
2017-03-17 19:17:56 +0100

    Added CORS headers

--- de.cweyermann.btc.server/src/main/java/de/cweyermann/btc/server/Startup.java
+++ de.cweyermann.btc.server/src/main/java/de/cweyermann/btc/server/Startup.java
@@ -5,6 +5,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import de.cweyermann.btc.server.boundary.rest.AbstractRestControl;
 import de.cweyermann.btc.server.boundary.rest.ShowClubs;
 import de.cweyermann.btc.server.boundary.rest.ShowDisciplines;
 import de.cweyermann.btc.server.boundary.rest.ShowGroup;
@@ -15,65 +16,84 @@ import de.cweyermann.btc.server.boundary.tpfile.GetGroups;
 import de.cweyermann.btc.server.boundary.tpfile.TpFileConnectionInvalid;
 import de.cweyermann.btc.server.control.CalculateClubStandings;
 import de.cweyermann.btc.server.control.CalculateGroupStandings;
+import de.cweyermann.btc.server.entity.AbstractEntity;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
+import io.vertx.core.Handler;
+import io.vertx.core.MultiMap;
+import io.vertx.core.http.HttpMethod;
 import io.vertx.core.http.HttpServer;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.CorsHandler;
 
 // TODO should be refactored to Microservices
 public class Startup extends AbstractVerticle {
 
-	private ShowDisciplines showDisciplines;
-	private ShowGroupOverview showgroupOverview;
-	private ShowClubs showClubs;
-	private ShowGroup showGroup;
+    private ShowDisciplines showDisciplines;
+    private ShowGroupOverview showgroupOverview;
+    private ShowClubs showClubs;
+    private ShowGroup showGroup;
 
-	@Override
-	public void start(Future<Void> startFuture) throws Exception {
-		String filePath = config().getString("tpfile.path");
-		Integer port = config().getInteger("httpserver.port");
+    @Override
+    public void start(Future<Void> startFuture) throws Exception {
+        String filePath = config().getString("tpfile.path");
+        Integer port = config().getInteger("httpserver.port");
 
-		Connection dbConnection = makeConnection(filePath);
-		init(dbConnection);
+        Connection dbConnection = makeConnection(filePath);
+        init(dbConnection);
 
-		startHttpServer(port, startFuture);
-	}
+        startHttpServer(port, startFuture);
+    }
 
-	private Connection makeConnection(String filePath) {
-		try {
-			Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
+    private Connection makeConnection(String filePath) {
+        try {
+            Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
 
-			File accessFile = new File(filePath);
-			if (!accessFile.exists()) { throw new TpFileConnectionInvalid("File " + filePath + " is invalid!"); }
+            File accessFile = new File(filePath);
+            if (!accessFile.exists()) {
+                throw new TpFileConnectionInvalid("File " + filePath + " is invalid!");
+            }
 
-			return DriverManager.getConnection("jdbc:ucanaccess://" + filePath, "", "d4R2GY76w2qzZ");
-		} catch (ClassNotFoundException | SQLException e) {
-			throw new TpFileConnectionInvalid(e);
-		}
-	}
+            return DriverManager.getConnection("jdbc:ucanaccess://" + filePath, "",
+                    "d4R2GY76w2qzZ");
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new TpFileConnectionInvalid(e);
+        }
+    }
 
-	private void startHttpServer(Integer port, Future<Void> startFuture) {
-		HttpServer server = vertx.createHttpServer();
-		Router router = Router.router(vertx);
+    private void startHttpServer(Integer port, Future<Void> startFuture) {
+        Router router = Router.router(vertx);
 
-		router.route("/btc/clubs/").handler(showClubs);
-		router.route("/btc/disciplines/").handler(showDisciplines);
-		router.route("/btc/disciplines/:disciplines").handler(showgroupOverview);
-		router.route("/btc/groups/:group").handler(showGroup);
+        CorsHandler corsHandler = CorsHandler.create("*");
+        corsHandler.allowedMethod(HttpMethod.OPTIONS);
+        corsHandler.allowedMethod(HttpMethod.GET);
+        corsHandler.allowedHeader("Content-Type");
 
-		server.requestHandler(router::accept).listen(port, x -> startFuture.complete());
-	}
+        registerRoute("clubs", showClubs, router, corsHandler);
+        registerRoute("disciplines", showDisciplines, router, corsHandler);
+        registerRoute("disciplines/:disciplines", showgroupOverview, router, corsHandler);
+        registerRoute("groups/:group", showGroup, router, corsHandler);
 
-	private void init(Connection dbConnection) {
-		GetDisciplines getDisciplines = new GetDisciplines(dbConnection);
-		GetGroup getGroup = new GetGroup(dbConnection);
-		GetGroups getGroups = new GetGroups(dbConnection);
-		CalculateGroupStandings calculateGroup = new CalculateGroupStandings();
-		CalculateClubStandings calculateClub = new CalculateClubStandings(10, 6, 4, 2, 1);
+        HttpServer server = vertx.createHttpServer();
+        server.requestHandler(router::accept).listen(port, x -> startFuture.complete());
+    }
 
-		showDisciplines = new ShowDisciplines(getDisciplines);
-		showgroupOverview = new ShowGroupOverview(getGroups, calculateGroup, getGroup);
-		showClubs = new ShowClubs(getGroups, calculateClub, getGroup);
-		showGroup = new ShowGroup(getGroup, calculateGroup);
-	}
+    private void registerRoute(String name, AbstractRestControl<? extends AbstractEntity> handler,
+            Router router, CorsHandler corsHandler) {
+        router.route("/btc/" + name).handler(corsHandler);
+        router.route("/btc/" + name).handler(handler);
+    }
+
+    private void init(Connection dbConnection) {
+        GetDisciplines getDisciplines = new GetDisciplines(dbConnection);
+        GetGroup getGroup = new GetGroup(dbConnection);
+        GetGroups getGroups = new GetGroups(dbConnection);
+        CalculateGroupStandings calculateGroup = new CalculateGroupStandings();
+        CalculateClubStandings calculateClub = new CalculateClubStandings(10, 6, 4, 2, 1);
+
+        showDisciplines = new ShowDisciplines(getDisciplines);
+        showgroupOverview = new ShowGroupOverview(getGroups, calculateGroup, getGroup);
+        showClubs = new ShowClubs(getGroups, calculateClub, getGroup);
+        showGroup = new ShowGroup(getGroup, calculateGroup);
+    }
 }
