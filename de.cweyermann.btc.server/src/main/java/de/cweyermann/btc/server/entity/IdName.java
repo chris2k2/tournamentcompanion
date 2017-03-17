35de65950bb54760242e67a31093be4d7b577901
chris <github@cweyermann.de>
2017-03-17 20:15:39 +0100

    implemented simpler access for spa

---
+++ de.cweyermann.btc.server/src/main/java/de/cweyermann/btc/server/entity/IdName.java
@@ -0,0 +1,25 @@
+package de.cweyermann.btc.server.entity;
+
+public class IdName {
+
+    private String name;
+    
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
